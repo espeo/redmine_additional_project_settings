cdab7114811bc6f3a238486f20f0391447ded695
Jacek Tomaszewski <jacek@jtom.me>
2014-06-21 19:06:42 +0200

    Avoid conflict with project#start_date method

--- lib/models/project_patch_2.rb
+++ lib/models/project_patch_2.rb
@@ -9,8 +9,8 @@ module ProjectPatch2
   end
   
   module InstanceMethods
-    def start_date
-      @start_date ||= begin
+    def custom_start_date
+      @custom_start_date ||= begin
         date_value = CustomValue.where(
           custom_field_id: Espeo::CustomFields::DEFAULTS[:project_start_date][:id], 
           customized_id: self.id, 
@@ -20,12 +20,12 @@ module ProjectPatch2
       end
     end
 
-    def end_date
-      @end_date ||= begin
+    def custom_end_date
+      @custom_end_date ||= begin
         date_value = CustomValue.where(
-          custom_field_id: Espeo::CustomFields::DEFAULTS[:project_end_date][:id], 
-          customized_id: self.id, 
-          customized_type: self.class.name
+          custom_field_id: Espeo::CustomFields::DEFAULTS[:project_start_date][:id], 
+          customized_id: p.id, 
+          customized_type: p.class.name
         ).pluck(:value).first
         Date.parse(date_value) if date_value
       end
